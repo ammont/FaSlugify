0fed36076fd0201b065d8ed301dd31b4fcfdffa1
Amir Montazer <ammontazer@gmail.com>
2014-07-27 15:57:59 +0430

    Used finglify for translation and added it as a dependency

--- src/FaSlugify.php
+++ src/FaSlugify.php
@@ -23,55 +23,6 @@ class FaSlugify {
 	/** @var boolean shows if the text should be translated */
 	private $translate;
 
-    /** @var array */
-    private $rules = array(
-        // Numeric characters
-        '۱' => 1,
-        '۲' => 2,
-        '۳' => 3,
-        '۴' => 4,
-        '۵' => 5,
-        '۶' => 6,
-        '۷' => 7,
-        '۸' => 8,
-        '۹' => 9,
-        '۰' => 0,
-
-        /* Persian */
-        'آ' => 'aa',
-        'ا' => 'a',
-        'ب' => 'b',
-        'پ' => 'p',
-        'ت' => 't',
-        'ث' => 's',
-        'ج' => 'j',
-        'چ' => 'ch',
-        'ح' => 'h',
-        'خ' => 'kh',
-        'د' => 'd',
-        'ذ' => 'z',
-        'ر' => 'r',
-        'ز' => 'z',
-        'س' => 's',
-        'ش' => 'sh',
-        'ص' => 's',
-        'ض' => 'z',
-        'ط' => 't',
-        'ظ' => 'z',
-        'ع' => 'aa',
-        'غ' => 'gh',
-        'ف' => 'f',
-        'ق' => 'gh',
-        'ك' => 'k',
-        'گ' => 'g',
-        'ل' => 'l',
-        'م' => 'm',
-        'ن' => 'n',
-        'و' => 'v',
-        'ه' => 'h',
-        'ي' => 'y'
-    );
-
     /**
      * The class constructor.
      *
@@ -94,7 +45,10 @@ class FaSlugify {
     {
     	if ($this->translate)
     	{
-    		$string = strtolower(strtr($string, $this->rules));
+    		$finglify = new Finglify();
+
+    		$string = $finglify->translate($string);
+    		$string = strtolower($string);
         	$string = preg_replace('/([^a-z0-9]|-)+/', $separator, $string);
         	$string = strtolower($string);
     	}
